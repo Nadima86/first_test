bb4ef4d716d282b80c72b3b8bd0ca4cbcd9833d5
Nadima <103410488+Nadima86@users.noreply.github.com>
2022-04-13 14:21:14 +0600

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,4 @@
+n=7 
+for i in range(5):
+    n = n*n
+    print(n)
---
+++ lesson4.py
@@ -0,0 +1,4 @@
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+for len in range (len(languages)):
+    print(len,languages[len])
+
